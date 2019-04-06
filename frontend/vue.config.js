12f577fa250f402c43c7c2d4235b68f248fa14a5
Tadek Teleżyński <tadekte@gmail.com>
2019-04-06 21:07:43 +0200

    Add custom service worker

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,4 +10,13 @@ module.exports = {
   //     },
   //   },
   // }
+  pwa: {
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'public/service-worker.js',
+      // ...other Workbox options...
+    }
+  }
 };
